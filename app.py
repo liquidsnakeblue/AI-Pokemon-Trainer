3f49c73cf604087851e750a64181024d5193daa1
liuzihao <liuzihao@qdzx.net.cn>
2025-01-02 10:42:07 +0800

    [fix]

--- app.py
+++ app.py
@@ -86,4 +86,4 @@ def load_progress():
 if __name__ == '__main__':
     log = logging.getLogger('werkzeug')
     log.disabled = True
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
