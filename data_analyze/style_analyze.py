a2e1e797266a8a9129c0cada8535ee6277bc2f15
Edgar Liu <liuzihao@qdzx.net.cn>
2025-05-21 22:01:53 +0800

    [fix]

--- data_analyze/style_analyze.py
+++ data_analyze/style_analyze.py
@@ -7,25 +7,45 @@ import matplotlib.pyplot as plt
 from scipy import stats
 
 
+def get_std_decision(tmp):
+    if tmp[0] == "i":
+        return "item"
+    
+    if tmp[0] == "s":
+        return "switch"
+    
+    if tmp == "run":
+        return "run"
+    
+    return f"move{tmp}"
+
 
 def process_model_data(model_list):
 
-    category_name = []
+    category_name = [
+        "move1",
+        "move2",
+        "move3",
+        "move4",
+        "item",
+        "switch",
+        "run",
+    ]
     result = {
-        "test_list0": [] , 
-        "test_list1": [] ,
-        "test_list2": [] ,
-        "test_list3": [],
-        "test_list4": [],
-        "test_list5" :[],
-        "test_list6": []
+        "test_list0": [0, 0, 0, 0, 0, 0, 0], 
+        "test_list1": [0, 0, 0, 0, 0, 0, 0],
+        "test_list2": [0, 0, 0, 0, 0, 0, 0],
+        "test_list3": [0, 0, 0, 0, 0, 0, 0],
+        "test_list4": [0, 0, 0, 0, 0, 0, 0],
+        "test_list5" :[0, 0, 0, 0, 0, 0, 0],
+        "test_list6": [0, 0, 0, 0, 0, 0, 0]
     }
     
     for i in range(len(model_list)-1):
         for json_data in model_list[i]:
             
             for item in json_data:
-                decision = item["last_operation"]["decision"]
+                decision = get_std_decision(item["last_operation"]["decision"])
                 if decision not in category_name:
                     category_name.append(decision)
                     for key in result:
@@ -36,7 +56,7 @@ def process_model_data(model_list):
                     result[f"test_list{i}"][num] = result[f"test_list{i}"][num] + 1 
 
                 for tmp in item["rounds"][-1]["operation_history"]: 
-                    operation = tmp["operation"]
+                    operation = get_std_decision(tmp["operation"])
 
                     if operation in category_name:
                         c = category_name.index(operation)
@@ -87,7 +107,6 @@ with open(list_path, "r", encoding="utf-8") as file:
     print(len(model_list))
 category_names, results = process_model_data(model_list)
 
-plt.show()
 
 
 """category_names = ['Strongly disagree', 'Disagree',
@@ -102,14 +121,14 @@ results = {
 }"""
 
 
-def survey(results, category_names):
+def survey(results, category_names, min_width_for_label=0.05):
     for key, value in results.items():
         tmp = np.sum(value)
         for i in range(len(value)):
             results[key][i] = results[key][i] / tmp
 
-    labels = list(results.keys())
-    data = np.array(list(results.values()))
+    labels = topics_list
+    data = np.round(np.array(list(results.values())), decimals=2)
     data_cum = data.cumsum(axis=1)
     category_colors = plt.colormaps['RdYlGn'](
         np.linspace(0.15, 0.85, data.shape[1]))
@@ -122,16 +141,47 @@ def survey(results, category_names):
     for i, (colname, color) in enumerate(zip(category_names, category_colors)):
         widths = data[:, i]
         starts = data_cum[:, i] - widths
-        rects = ax.barh(labels, widths, left=starts, height=0.5,
-                        label=colname, color=color)
-
+        mask = widths > 0
+        filtered_labels = [lab for lab, m in zip(labels, mask) if m]
+        filtered_widths = widths[mask]
+        filtered_starts = starts[mask]
+        
+        rects = ax.barh(filtered_labels, filtered_widths, 
+                        left=filtered_starts, 
+                        height=0.5,
+                        label=colname, 
+                        color=color)
         r, g, b, _ = color
         text_color = 'white' if r * g * b < 0.5 else 'darkgrey'
-        ax.bar_label(rects, label_type='center', color=text_color)
+        for rect, width in zip(rects, filtered_widths):
+            x = rect.get_x() + rect.get_width() / 2
+            y = rect.get_y() + rect.get_height() / 2
+            
+            ax.text(x, y, f'{width:.0%}',
+                    ha='center', va='center',
+                    color=text_color, fontsize=8)
+
     ax.legend(ncols=len(category_names), bbox_to_anchor=(0, 1),
               loc='lower left', fontsize='small')
-    plt.show()
+    # plt.show()
     return fig, ax
+
+CATEGORY_FRIEND_NAMES = {
+    "move1": "Move 1",
+    "move2": "Move 2",
+    "move3": "Move 3",
+    "move4": "Move 4",
     
+    "switch": "Switch",
+    "item": "Item",
+    "run": "Run",
+}
+
+for i in range(len(category_names)):
+    category_names[i] = CATEGORY_FRIEND_NAMES[category_names[i]]
 
 survey(results, category_names)
+
+# plt.show()
+
+plt.savefig('output.png', dpi=300, bbox_inches='tight', transparent=True)
